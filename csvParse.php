18c10f76d3d5e0534663762409378e307af31f05
Pratanu Khajanchi <pratanukhajanchi@gmail.com>
2020-07-10 17:28:22 +0530

    1st phase

--- csvParse.php
+++ csvParse.php
@@ -5,15 +5,15 @@
 
             //if there was an error uploading the file
         if ($_FILES["file"]["error"] > 0) {
-            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-
+            echo "No file selected <br />";
         }
         else {
+            header('Location: data.html');//to show the datatable
                 //Print file details
             echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-            echo "Type: " . $_FILES["file"]["type"] . "<br />";
+           // echo "Type: " . $_FILES["file"]["type"] . "<br />";
             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-            echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+           // echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
 
                 //if file already exists
             if (file_exists("upload/" . $_FILES["file"]["name"])) {
@@ -24,53 +24,36 @@
             $storagename = "uploaded_file.csv";
             move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $storagename);
             echo "Stored in: " . "upload/" . $storagename . "<br />";
+            $storagename= "upload/" . $storagename;
             }
         }
     } else {
             echo "No file selected <br />";
     }
     }
+    jj_readcsv($storagename,$header=true);
 
-    if ( isset($storagename) && $file = fopen( "upload/" . $storagename , "r" ) ) {
-
-        echo "File opened.<br />";
-
-        $firstline = fgets ($file, 4096 );
-            //Gets the number of fields, in CSV-files the names of the fields are mostly given in the first line
-        $num = strlen($firstline) - strlen(str_replace(";", "", $firstline));
-
-            //save the different fields of the firstline in an array called fields
-        $fields = array();
-        $fields = explode( ";", $firstline, ($num+1) );
-
-        $line = array();
-        $i = 0;
-
-            //CSV: one line is one record and the cells/fields are seperated by ";"
-            //so $dsatz is an two dimensional array saving the records like this: $dsatz[number of record][number of cell]
-        while ( $line[$i] = fgets ($file, 4096) ) {
-
-            $dsatz[$i] = array();
-            $dsatz[$i] = explode( ";", $line[$i], ($num+1) );
-
-            $i++;
-        }
-
-            echo "<table>";
-            echo "<tr>";
-        for ( $k = 0; $k != ($num+1); $k++ ) {
-            echo "<td>" . $fields[$k] . "</td>";
-        }
-            echo "</tr>";
-
-        foreach ($dsatz as $key => $number) {
-                    //new table row for every record
-            echo "<tr>";
-            foreach ($number as $k => $content) {
-                            //new table cell for every field of the record
-                echo "<td>" . $content . "</td>";
+    function jj_readcsv($filename, $header=false) {
+        $handle = fopen($filename, "r");
+        echo '<table>';
+        //display header row if true
+        if ($header) {
+            $csvcontents = fgetcsv($handle);
+            echo '<tr>';
+            foreach ($csvcontents as $headercolumn) {
+                echo "<th>$headercolumn</th>";
             }
+            echo '</tr>';
         }
-        echo "</table>";
+        // displaying contents
+        while ($csvcontents = fgetcsv($handle)) {
+            echo '<tr>';
+            foreach ($csvcontents as $column) {
+                echo "<td>$column</td>";
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+        fclose($handle);
     }
 ?>
