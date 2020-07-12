62e5847ea1a76ef15493b821ecb07f8ddfecb8e6
Pratanu Khajanchi <pratanukhajanchi@gmail.com>
2020-07-12 17:21:01 +0530

    NavBar added

--- index.php
+++ index.php
@@ -12,19 +12,25 @@
   <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <style>
-  .box
-  {
-   max-width:600px;
-   width:100%;
-   margin: 0 auto;;
-  }
-  </style>
+  
  </head>
  <body>
   <div class="container">
    <br />
    <h2 align="center">Joe Stock Exchange</h2>
+   <br />
+   <br />
+   <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="about.php">JSE</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="trade.php">Trading</a></li>
+    </ul>
+  </div>
+</nav>
    <br />
    <br />
    <form id="upload_csv" method="post" enctype="multipart/form-data">
---
+++ trade.php
@@ -0,0 +1,39 @@
+
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Joe Stock Exchange</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h2 align="center">Joe Stock Exchange</h2>
+   <br />
+   <br />
+   <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="about.php">JSE</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="#">Trading</a></li>
+    </ul>
+  </div>
+</nav>
+
+<h3>Lets Trade</h3>
+
+
+ 
+</html>
